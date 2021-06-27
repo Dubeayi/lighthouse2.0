c99e6a2855a716cd9834af4622e7baca8cc38fcd
Anar Chalk <annachak9@gmail.com>
2021-06-27 15:10:27 -0400

    created social media component

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 // nodejs library that concatenates strings
 import classnames from "classnames";
+import SectionSocialMedia from "./SectionSocialMedia";
 // reactstrap components
 import {
   Button,
@@ -14,7 +15,7 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownItem,
-  DropdownMenu
+  DropdownMenu,
 } from "reactstrap";
 
 function IndexNavbar() {
@@ -80,109 +81,50 @@ function IndexNavbar() {
           isOpen={navbarCollapse}
         >
           <Nav navbar>
-
-<UncontrolledDropdown nav inNavbar>
-                      <DropdownToggle
-                        aria-expanded={false}
-                        aria-haspopup={true}
-                        caret
-                        color="default"
-                        data-toggle="dropdown"
-                        href="#pablo"
-                        id="dropdownMenuButton"
-                        nav
-                        onClick={(e) => e.preventDefault()}
-                        role="button"
-                      >
-                       <i className="nc-icon nc-book-bookmark" /> Menu Items
-                      </DropdownToggle>
-                      <DropdownMenu
-                        aria-labelledby="dropdownMenuButton"
-                        className="dropdown-info"
-                      >
-                        <DropdownItem header tag="span">
-                          Dropdown header
-                        </DropdownItem>
-                        <DropdownItem
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Services
-                        </DropdownItem>
-                        <DropdownItem
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Another action
-                        </DropdownItem>
-                        <DropdownItem
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Something else here
-                        </DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Separated link
-                        </DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          Another separated link
-                        </DropdownItem>
-                      </DropdownMenu>
-                    </UncontrolledDropdown>
-
-
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="#"
-                target="_blank"
-                title="Follow us on Twitter"
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle
+                aria-expanded={false}
+                aria-haspopup={true}
+                caret
+                color="default"
+                data-toggle="dropdown"
+                href="#pablo"
+                id="dropdownMenuButton"
+                nav
+                onClick={(e) => e.preventDefault()}
+                role="button"
               >
-                <i className="fa fa-twitter" />
-                <p className="d-lg-none">Twitter</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="#"
-                target="_blank"
-                title="Like us on Facebook"
+                <i className="nc-icon nc-book-bookmark" /> Menu Items
+              </DropdownToggle>
+              <DropdownMenu
+                aria-labelledby="dropdownMenuButton"
+                className="dropdown-info"
               >
-                <i className="fa fa-facebook-square" />
-                <p className="d-lg-none">Facebook</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="#"
-                target="_blank"
-                title="Follow us on Instagram"
-              >
-                <i className="fa fa-instagram" />
-                <p className="d-lg-none">Instagram</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="#"
-                target="_blank"
-                title="Star on GitHub"
-              >
-                <i className="fa fa-github" />
-                <p className="d-lg-none">GitHub</p>
-              </NavLink>
-            </NavItem>
+                <DropdownItem header tag="span">
+                  Dropdown header
+                </DropdownItem>
+                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                  Services
+                </DropdownItem>
+                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                  Another action
+                </DropdownItem>
+                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                  Something else here
+                </DropdownItem>
+               
+                <DropdownItem divider />
+                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                  Separated link
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                  Another separated link
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <SectionSocialMedia/>
+    
             <NavItem>
               <Button
                 className="btn-round"
