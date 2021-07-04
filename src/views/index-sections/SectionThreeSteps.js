e8afb5c99353a8e8264e27fadf11ca74213ba19c
Dubeayi <dubeayi@gmail.com>
2021-07-04 12:35:13 -0700

    added 3 steps to Landing Page

--- src/views/index-sections/SectionThreeSteps.js
+++ src/views/index-sections/SectionThreeSteps.js
@@ -15,6 +15,22 @@ export default function ThreeSteps () {
             </h3>
           <span className="learnMore">Learn More</span>
         </div>
+        <div className="numberedSection">
+          <h3>
+            <span className="numero">2</span>
+
+            CONNECT WITH OUR TEAM
+            </h3>
+          <span className="learnMore">Call Us</span>
+        </div>
+        <div className="numberedSection">
+          <h3>
+            <span className="numero">3</span>
+
+            BEGIN THE JOURNEY TO GET UNSTUCK
+            </h3>
+          <span className="learnMore">Begin the process</span>
+        </div>
       </Container>
     </>
   )
