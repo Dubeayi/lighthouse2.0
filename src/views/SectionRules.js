dc533153a3555eac9cbcd169b332dd16f3543369
Anar Chalk <annachak9@gmail.com>
2021-07-22 19:37:15 -0400

    added navbar and footer in rules component

--- src/views/SectionRules.js
+++ src/views/SectionRules.js
@@ -1,94 +1,36 @@
 import React from "react";
 
-const SectionRules = () => {
+// reactstrap components
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import LandingPageTemplate from "components/Headers/LandingPageTemplate.js";
+import Footer from "components/Footers/Footer.js";
+
+import Rules from "./rules-sections/SectionRules.js";
+
+
+// index sections
+
+function Index() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("index");
+    return function cleanup() {
+      document.body.classList.remove("index");
+    };
+  });
   return (
     <>
-      <h1> SECURITY, RULES, EXPECTATIONS AND GUIDELINES</h1>
-      <h3>SECURITY</h3>
-      <p>
-        The Centre will provide adequate security to ensure the safety of its
-        client from outside interference , while also preventing unauthorised
-        exit from the premises by clients, and ensuring a drug-free environment.
-        The facility is surrounded by a fence, with a security post. Visitors
-        are properly screened before being allowed into the facility. Searching
-        for dangerous drugs and items are randomly conducted and documented.
-      </p>
-      <h3>RULES, EXPECTATIONS AND GUIDELINES</h3>
-      <ul>
-        <li>
-          1.{""}The consumption of alcohol or use of any unauthorized mind or
-          mood-altering substance is strictly prohibited, especially within
-          Lighthouse Lekki centre. Failure to comply with this rule will result
-          in immediate discharge
-        </li>
-        <li>
-          2. {""}Any resident who behaves in a manner that would cause suspicion
-          of the use of alcohol or mind or mood-altering substances or who is
-          found to have any alcohol, drugs or paraphernalia related to drug use,
-          is subject to discharge.
-        </li>
-        <li>
-          3. All prescription medications, over the counter drugs or vitamins of
-          any kind, are to be deposited with the admitting nurse and will be
-          dispensed as prescribed, on request.
-        </li>
-        <li>
-          4. All residents are required to take an active part in the program.
-          Therefore, attendance at all centre meetings is mandatory.
-        </li>
-        <li>5. Punctuality is required for all meals and meetings.</li>
-        <li>
-          6. We will not allow tobacco use in program facilities or on grounds
-        </li>
-        <li>
-          7. Bedrooms are to be kept neat and tidy always. The wakeup call is
-          between 5:45-6:00 AM. Beds must be made by 8:00 AM. All books,
-          toiletries, face cloths, towels, personal items and clothing must be
-          kept in their proper place. Residents retire at 11:00 PM. All lights
-          out at 11:30 PM.
-        </li>
-        <li>
-          8. Residents are required to be neat in appearance always. Exercise
-          apparel cannot be substituted for casual wear in the house.
-        </li>
-        <li>
-          9. No food OR beverages are allowed in any of the bedrooms. All food
-          is to be consumed in the designated areas.
-        </li>
-        <li>
-          10. Residents maintain the cleanliness of their room and of the
-          centre. Residents shall take all reasonable steps to ensure
-          cleanliness, including, but not limited to: shutting off all lights,
-          ensuring that refuse is properly disposed of. Common areas are to
-          remain neat and clean. This includes common living rooms and bathrooms
-        </li>
-        <li>
-          11. The main kitchen area, fridge, cooker and microwave are off-limits
-          to all residents expect those who have been assigned kitchen duties.
-        </li>
-        <li>
-          12. Fire/emergency evacuation procedures; In the event of fire or
-          emergency alarm, clients should leave the building via the closest
-          exit and proceed directly to the front of the property. A staff member
-          will call roll to make sure all are present. A staff member will
-          notify you when it is safe to return to the building.
-        </li>
-        <li>
-          13. Client Conduct; Violence, threats of violence or verbal abuse will
-          not be tolerated, and may result in immediate discharge from
-          Lighthouse Lekki. You are expected to respect staff and fellow
-          residents always during your stay at the facility.
-        </li>
-        <li>
-          14. Inspection of Personal Belongings; At the point of admission, the
-          client’s belongings will be inspected by a member of Lighthouse Lekki
-          staff. Items removed will be locked in a secure location. Lighthouse
-          Lekki reserves the right to inspect personal belongings and space
-          provided to the client (i.e. bedroom) at any time
-        </li>
-      </ul>
+      <IndexNavbar />
+      <LandingPageTemplate title="Rules" />
+      <div className="main" style={{ backgroundColor: "floralwhite" }}>
+        <Rules />
+        <Footer />
+      </div>
     </>
   );
-};
+}
 
-export default SectionRules;
+
+export default Index;
