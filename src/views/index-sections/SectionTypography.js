f65568eb97604e98413f6ddf55e938108a48aff6
Dubeayi <dubeayi@gmail.com>
2021-07-23 16:25:26 -0700

    Completed Reviews Section

--- src/views/index-sections/SectionTypography.js
+++ src/views/index-sections/SectionTypography.js
@@ -12,47 +12,35 @@ function SectionTypography() {
         <div id="images">
           <Container>
             <div className="title">
-              <h3>Possible Client Reviews?</h3>
+              <h3 style={{textAlign: "center", paddingBottom: "15px"}}>Client Reviews</h3>
             </div>
-            <Row>
-              <Col md="3" sm="6">
-                <h4 className="images-title">Rounded Image</h4>
-                <img
-                  alt="..."
-                  className="img-rounded img-responsive"
-                  src="https://pbs.twimg.com/media/EkC1LR2XkAAYsmQ.jpg"
-                />
-                <div className="img-details">
-                  <div className="author">
-                    <img
-                      alt="..."
-                      className="img-circle img-no-padding img-responsive"
-                      src="https://static.yafri.ca/wp-content/uploads/2016/11/10054619/President-Buhari.jpg"
-                    />
-                  </div>
-                  <p>
-                    Each meal was prepared with much care. Too bad I am a joker
-                    <br />-Mohamadu Buhari
-                  </p>
-                </div>
-              </Col>
-              <Col className="mr-auto ml-auto" md="2" sm="3">
-                <h4 className="images-title">Circle Image</h4>
+            <Row style={{marginBottom: "11em"}}>
+              <Col className="mr-auto ml-auto ipad" style={{height: "fit-content"}} md="2" sm="3">
                 <img
                   alt="..."
                   className="img-circle img-no-padding img-responsive"
-                  src={require("assets/img/faces/kaci-baum-2.jpg").default}
+                  src={require("assets/img/man1Blurred.jpg").default}
+                  style={{height: "15em"}}
                 />
-                <p className="text-center">Client B</p>
+                <p className="textLeft"><em>Deciding to go to Lighthouse for my weed addiction is probably the healthiest decision I could have made for my career. The staff were so encouraging! As soon as I entered, I knew I was in the right place</em></p>
               </Col>
-              <Col className="mr-auto" md="2" sm="3">
-                <h4 className="images-title">Thumbnail</h4>
+              <Col className="mr-auto ml-auto ipad" style={{height: "fit-content"}}  md="2" sm="3">
                 <img
                   alt="..."
-                  className="img-thumbnail img-responsive"
-                  src="https://www.oozlemedia.com/wp-content/uploads/2016/10/donald-trump-joker-makeup-batman.jpg"
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("assets/img/man2Blurred.png").default}
+                  style={{height: "15em"}}
                 />
-                <p className="text-center">D.T</p>
+                <p className="textLeft"><em>I initially didn't want to go to Lighthouse, but was forced by my family. And boy was it the best decision for me! I didn't realize it then, but I was on a crash course to destruction. I knew I needed help, and always going to be grateful to Olawale for believing in me when I didn't believe in myself!</em></p>
+              </Col>
+              <Col className="mr-auto ml-auto ipad" style={{height: "fit-content"}}  md="2" sm="3">
+                <img
+                  alt="..."
+                  className="img-circle img-no-padding img-responsive"
+                  src={require("assets/img/man3Blurred.jpg").default}
+                  style={{height: "15em"}}
+                />
+                <p className="textLeft"><em>I am so sorry it has taken me a while to leave this review, but I have been so caught up in my 'new' life. That is, the life that I have chosen after leaving Lighthouse, where I am in control. For the first time since I was a teenager I actually know the happpiness that comes with being truly free. I love you Lighthouse!!!</em></p>
               </Col>
             </Row>
           </Container>
