0a37f1553f019a929d25ae76668156629f3686b3
Anar Chalk <annachak9@gmail.com>
2021-07-24 01:33:51 -0400

    section divider icon styling done for mobile view

--- src/views/index-sections/SectionDivider.js
+++ src/views/index-sections/SectionDivider.js
@@ -1,9 +1,7 @@
 import React from "react";
-import hand from '../../assets/img/divider/helping-hand.svg';
-import growth from '../../assets/img/divider/growth.svg';
-import yinyang from '../../assets/img/divider/handshake.svg';
-
-
+import hand from "../../assets/img/divider/helping-hand.svg";
+import growth from "../../assets/img/divider/growth.svg";
+import yinyang from "../../assets/img/divider/handshake.svg";
 
 // reactstrap components
 import { Container } from "reactstrap";
@@ -23,9 +21,28 @@ function SectionDivider() {
         // }}
       >
         {/* <Container> */}
-        <div className="child-divider"><img className="divider-img" src={hand} alt="handshake"/><h3>Reach Out</h3></div>
-        <div className="child-divider"><img className="divider-img" src={growth} alt= "a plant in hand"/><h3>Find Meaning</h3></div>
-        <div className="child-divider"><img className="divider-img" src={yinyang} alt="yin-yang symbol in head"/><h3>Find Healing</h3></div>
+        <div className="child-divider">
+          <div className="icon-wrapper">
+            <img className="divider-img" src={hand} alt="handshake" />
+          </div>
+          <h3>Reach Out</h3>
+        </div>
+        <div className="child-divider">
+          <div className="icon-wrapper">
+            <img className="divider-img" src={growth} alt="a plant in hand" />
+          </div>
+          <h3>Find Meaning</h3>
+        </div>
+        <div className="child-divider">
+          <div className="icon-wrapper">
+            <img
+              className="divider-img"
+              src={yinyang}
+              alt="yin-yang symbol in head"
+            />
+          </div>
+          <h3>Find Healing</h3>
+        </div>
       </div>
       {/* <div className="section section-buttons color2" style={{backgroundColor: "rgb(119 122 89 / 51%)"}}>#2</div>
       <div className="section section-buttons color3"style={{backgroundColor: "rgb(201 145 85 / 51%)"}}>#3</div> */}
