180d5bbfafc97510144ba9776ac968a79336c592
Anar Chalk <annachak9@gmail.com>
2021-07-24 00:38:19 -0400

    section divider mobile view styling done

--- src/views/index-sections/SectionDivider.js
+++ src/views/index-sections/SectionDivider.js
@@ -1,4 +1,9 @@
 import React from "react";
+import hand from '../../assets/img/divider/helping-hand.svg';
+import growth from '../../assets/img/divider/growth.svg';
+import yinyang from '../../assets/img/divider/handshake.svg';
+
+
 
 // reactstrap components
 import { Container } from "reactstrap";
@@ -7,20 +12,20 @@ function SectionDivider() {
   return (
     <>
       <div
-        className="divider"
-        style={{
-          backgroundColor: "#5c8695",
-          //   marginLeft: "auto",
-          //   marginRight: "auto",
-          //   maxWidth: "1200px",
-          //   paddingLeft: "10px",
-          //   paddingRight: "10px",
-        }}
+        className="parent-divider"
+        // style={{
+        //   //backgroundColor: "#5c8695",
+        //   //   marginLeft: "auto",
+        //   //   marginRight: "auto",
+        //   //   maxWidth: "1200px",
+        //   //   paddingLeft: "10px",
+        //   //   paddingRight: "10px",
+        // }}
       >
         {/* <Container> */}
-        <div className="column-container"></div>
-        <div className="column-container"></div>
-        <div className="column-container"></div>
+        <div className="child-divider"><img className="divider-img" src={hand} alt="handshake"/><h3>Reach Out</h3></div>
+        <div className="child-divider"><img className="divider-img" src={growth} alt= "a plant in hand"/><h3>Find Meaning</h3></div>
+        <div className="child-divider"><img className="divider-img" src={yinyang} alt="yin-yang symbol in head"/><h3>Find Healing</h3></div>
       </div>
       {/* <div className="section section-buttons color2" style={{backgroundColor: "rgb(119 122 89 / 51%)"}}>#2</div>
       <div className="section section-buttons color3"style={{backgroundColor: "rgb(201 145 85 / 51%)"}}>#3</div> */}
