f4dabe1f5ad739d63de663a5eb0edc12baa6d4c6
Dubeayi <dubeayi@gmail.com>
2021-08-02 10:19:43 -0700

    fixing homePage loading issue2

--- src/views/index-sections/SectionIntroduction.js
+++ src/views/index-sections/SectionIntroduction.js
@@ -3,7 +3,6 @@ import React from "react";
 // reactstrap components
 import { Container } from "reactstrap";
 // import Aos from "aos";
-import "aos/dist/aos.css";
 import village from "assets/img/hug1.jpg";
 
 function SectionIntroduction() {
@@ -21,14 +20,14 @@ function SectionIntroduction() {
         }}
       >
         <Container>
-          <div className="title" data-aos="fade-up">
+          <div className="title">
             <h2 style={{ color: "#FFF", fontWeight: 400 }}>
               <span style={{ color: "#F5593D", fontWeight: 700 }}>You</span>{" "}
               have the power to change who you are
             </h2>
             <br />
           </div>
-          <div className="title" data-aos="fade-up">
+          <div className="title">
             <h2 style={{ color: "#FFF", fontWeight: 400 }}>
               <span style={{ color: "#F5593D", fontWeight: 700 }}>Our</span>{" "}
               purpose is to restore the dignity and vision back into the lives
@@ -36,15 +35,12 @@ function SectionIntroduction() {
             </h2>{" "}
             <br />
           </div>
-          <div data-aos="fade-up">
+          <div>
             <h2 style={{ color: "#FFF", fontWeight: 400 }}>
               <span style={{ color: "#F5593D", fontWeight: 700 }}>We</span> are
               here to support you on this journey, so that you don’t have to do
               it alone. You will smile again.
             </h2>
-            {/* <h5 className="textBody">
-              Our purpose is to restore the dignity and vision back into the lives of individuals, one mind
-              at a time. */}
           </div>
         </Container>
       </div>
